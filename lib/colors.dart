725c8af9f70bb028f3fa7d617c10ac7c2cc76165
dileep kumar <63330310+dileep9490@users.noreply.github.com>
2022-01-28 12:59:42 +0530

    created colors from hsl values

---
+++ lib/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+const Color blue = Color(0xff5847eb);
+const Color workColor = Color(0xffff8c66);
+const Color playColor = Color(0xff56c2e6);
+const Color studyColor = Color(0xffff5c7c);
+const Color exerciseColor = Color(0xff4acf81);
+const Color socialColor = Color(0xff7536d3);
+const Color selfcareColor = Color(0xfff1c65b);
+const Color veryDarkBlue = Color(0xff0f1424);
+const Color darkBlue = Color(0xff1c1f4a);
+const Color desaturatedBlue = Color(0xff6f76c8);
+const Color paleBlue = Color(0xffbdc1ff);
